681d432fd94e2188f03ea15090ecf2e809bb5263
Kirill <vardugin14@gmail.com>
2022-08-18 16:31:36 +0300

    Example002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите ваше имя "); //Выводим текст -> Введите ваше имя
+string username = Console.ReadLine(); // Добавляем тип данных в string текст -> " "
+Console.WriteLine("Привет, "); // Выводим текст -> Привет,
+Console.WriteLine(username); // Выводим текст, который в string username
+// Если написать просто Console.Write, то вывод будет в строку
